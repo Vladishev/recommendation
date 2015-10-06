03bcf56b04a1eeb18f20b45649cd4dead3feef04
Vlado <vladmsu@ukr.net>
2015-10-06 10:01:56 +0300

    3796 - code review done

--- app/code/local/Tim/Recommendation/controllers/IndexController.php
+++ app/code/local/Tim/Recommendation/controllers/IndexController.php
@@ -24,7 +24,8 @@ class Tim_Recommendation_IndexController extends Mage_Core_Controller_Front_Acti
     {
 
     }
-    public function opinionAction()
+
+    public function viewAction()
     {
         $this->loadLayout();
         $this->renderLayout();
