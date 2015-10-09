6e983d2b3376cfc2a06d89957cc03f4794ac62f9
bdn <bakalov.bogdan@gmail.com>
2015-10-09 18:04:01 +0300

    changed 3815 : multiply saving image & youtube url was added

--- app/code/local/Tim/Recommendation/controllers/IndexController.php
+++ app/code/local/Tim/Recommendation/controllers/IndexController.php
@@ -18,61 +18,60 @@ class Tim_Recommendation_IndexController extends Mage_Core_Controller_Front_Acti
     public function addAction()
     {
         $params = $this->getRequest()->getParams();
-        $customerId = $params['customer_id'];
-        $productId = $params['product_id'];
-        $opinionTitle = $params['opinion-title'];
-        $opinionAdvantages = $params['opinion-advantages'];
-        $opinionDisadvantages = $params['opinion-disadvantages'];
-        $opinionSummary = $params['opinion-summary'];
-        $itemValueToMoney = $params['itemValuetomoney'];
-        $itemDurability = $params['itemDurability'];
-        $itemFailure = $params['itemFailure'];
-        $itemEaseOfInstall = $params['itemEaseofinstall'];
-        $itemDoYouReccomend = $params['itemDoyoureccomend'];
-//        $linkToYoutube = $params['link_to_youtube'];
-//        $timEmail = $params['tim-email'];
-//        $timPassword = $params['tim-password'];
-//        $timNick = $params['tim-nick'];
+        $files = $this->reArrangeFiles($_FILES['tim-recommendation-img']);
+        $folderForFiles = Mage::getBaseDir('media') . DS . 'tim' . DS . 'recommendation';
+
+        if (!is_dir($folderForFiles)) {
+            mkdir($folderForFiles, 0777, true);
+        }
 
         $recommendationModel = Mage::getModel('tim_recommendation/recommendation')
-            ->setUserId($customerId)
-            ->setProductId($productId)
-            ->setTitle($opinionTitle)
-            ->setAdvantages($opinionAdvantages)
-            ->setDefects($opinionDisadvantages)
-            ->setConclusion($opinionSummary)
-            ->setRatingPrice($itemValueToMoney)
-            ->setRatingDurability($itemDurability)
-            ->setRatingFailure($itemFailure)
-            ->setRatingService($itemEaseOfInstall)
-            ->setRecomend($itemDoYouReccomend);
+            ->setUserId($params['customer_id'])
+            ->setProductId($params['product_id'])
+            ->setTitle($params['opinion-title'])
+            ->setAdvantages($params['opinion-advantages'])
+            ->setDefects($params['opinion-disadvantages'])
+            ->setConclusion($params['opinion-summary'])
+            ->setRatingPrice($params['itemValuetomoney'])
+            ->setRatingDurability($params['itemDurability'])
+            ->setRatingFailure($params['itemFailure'])
+            ->setRatingService($params['itemEaseofinstall'])
+            ->setRecomend($params['itemDoyoureccomend']);
         try {
             $recommendationModel->save();
+            $recomId = $recommendationModel->getRecomId();
             Mage::getSingleton('core/session')->addSuccess(Mage::helper('tim_recommendation')->__('Opinion was successfully added.'));
         } catch (Exception $e) {
             Mage::log($e->getMessage(), NULL, 'tim_recommendation.log');
             Mage::getSingleton('core/session')->addError(Mage::helper('tim_recommendation')->__('Can\'t add opinion.'));
         }
+
+        $mediaModel = Mage::getModel('tim_recommendation/media');
+        $mediaModel->setRecomId($recomId)
+            ->setName($params['link_to_youtube'])
+            ->setType('url/youtube');
+        $mediaModel->save();
+
+        foreach ($files as $file) {
+            $mediaModel = Mage::getModel('tim_recommendation/media')
+                ->setRecomId($recomId)
+                ->setName('/media/tim/recommendation/' . $file['name'])
+                ->setType($file['type']);
+            try {
+                $saveMedia = $mediaModel->save();
+            } catch (Exception $e) {
+                Mage::log($e->getMessage(), NULL, 'tim_recommendation.log');
+                Mage::getSingleton('core/session')->addError(Mage::helper('tim_recommendation')->__('Didn\'t save %s file.', $file['name']));
+            }
+            if ($saveMedia) {
+                if (isset($file['name']) && !empty($file['name'])) {
+                    $fileName = time() . $file['name'];
+                    $this->saveImage($fileName, $folderForFiles, $file);
+                }
+            }
+        }
+
         $this->_redirectReferer();
-//        Save img to table
-//        if (isset($_FILES['tim-recommendation-img']['name']) && $_FILES['tim-recommendation-img']['name'] != '') {
-//            try {
-//                $path = Mage::getBaseDir('media') . DS . 'tim' . DS . 'recommendation';
-//                if(!is_dir($path)){
-//                    mkdir($path, 0777, true);
-//                }
-//                $fileName = $_FILES['tim-recommendation-img']['name'] . time();
-//                $pathToFile = 'media' . DS . 'tim' . DS . 'recommendation' . DS . $fileName;
-//                $uploader = new Varien_File_Uploader('tim-recommendation-img');
-//                $uploader->setAllowedExtensions(array('jpg','jpeg','gif','png'));
-//                $uploader->setAllowRenameFiles(false);
-//                $uploader->setFilesDispersion(false);
-//                $uploader->save($path, $logoName);
-//
-//            } catch (Exception $e) {
-//
-//            }
-//        }
     }
 
     public function landingAction()
@@ -85,5 +84,40 @@ class Tim_Recommendation_IndexController extends Mage_Core_Controller_Front_Acti
     {
         $this->loadLayout();
         $this->renderLayout();
+    }
+
+    /**
+     * Save image to folder
+     * @param string $fileName
+     * @param string $path
+     * @param array $file
+     */
+    public function saveImage($fileName, $path, $file)
+    {
+        $uploader = new Varien_File_Uploader($file);
+        $uploader->setAllowedExtensions(array('png', 'gif', 'jpeg', 'jpg'));
+        $uploader->setAllowCreateFolders(true);
+        $uploader->setAllowRenameFiles(false);
+        $uploader->setFilesDispersion(false);
+        try {
+            $uploader->save($path, $fileName);
+        } catch (Exception $e) {
+            Mage::log($e->getMessage(), null, 'tim_recommendation.log');
+        }
+    }
+
+    /**
+     * Rearrange $_FILES array to clear array
+     * @param array $fileArray
+     * @return array $newFileArray
+     */
+    public function reArrangeFiles($fileArray)
+    {
+        foreach ($fileArray as $key => $all) {
+            foreach ($all as $i => $val) {
+                $newFileArray[$i][$key] = $val;
+            }
+        }
+        return $newFileArray;
     }
 }
