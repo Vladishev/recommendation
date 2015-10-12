12ef145265b6efa0a94d16ccd340af875d132d98
Vlado <vladmsu@ukr.net>
2015-10-12 14:02:38 +0300

    3821_vv_customer_view - CR

--- app/code/local/Tim/Recommendation/Block/Recommendation.php
+++ app/code/local/Tim/Recommendation/Block/Recommendation.php
@@ -9,5 +9,19 @@
  */
 class Tim_Recommendation_Block_Recommendation extends Mage_Core_Block_Template
 {
+    /* Gets user data from tables */
+    public function getUserInformation($userId)
+    {
+        $collection = Mage::getModel('tim_recommendation/user')->getCollection();
+        $collection->getSelect()
+            ->join(array('cev' => 'customer_entity_varchar'), 'cev.entity_id = main_table.customer_id', array('name' => 'value'))
+            ->join(array('cev1' => 'customer_entity_varchar'), 'cev1.entity_id = main_table.customer_id', array('surname' => 'value'))
+            ->join(array('ce' => 'customer_entity'), 'ce.entity_id = main_table.customer_id', 'email')
+            ->where('main_table.customer_id = ' . $userId . '')
+            ->where('cev.attribute_id = 5')
+            ->where('cev1.attribute_id = 7');
 
+        $userData = $collection->getFirstItem()->getData();
+        return $userData;
+    }
 }
