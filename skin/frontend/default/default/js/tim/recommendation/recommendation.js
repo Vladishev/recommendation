d49013a71f6ee1fdcf3cfe9fa84cb776d908bf33
bdn <bakalov.bogdan@gmail.com>
2015-11-20 10:43:16 +0200

    recom_038_01 code was refactored

--- skin/frontend/default/default/js/tim/recommendation/recommendation.js
+++ skin/frontend/default/default/js/tim/recommendation/recommendation.js
@@ -1,5 +1,5 @@
-jQuery(document).ready(function() {
-/* function to put value into html content right to rating stars and switch userlogin details*/
+jQuery(document).ready(function () {
+    /* function to put value into html content right to rating stars and switch userlogin details*/
 
     jQuery('input').on('change', function () {
         var inputChangeName = jQuery(this).attr('name');
@@ -8,7 +8,7 @@ jQuery(document).ready(function() {
         /* alert(inputChangeName+inputChangeValue)         */
         jQuery(inputChangeNameSpan).html(inputChangeValue);
         console.log(inputChangeName);
-        
+
         if (inputChangeName == 'userHaveaccount') {
             if (inputChangeValue == 'TAK') {
                 jQuery('.tim-userlogin-existuser').show();
@@ -23,45 +23,45 @@ jQuery(document).ready(function() {
 
 });
 
-    /* function to show place for adding comment */
-    function showAddComment() {
-        jQuery('#tim-comment-add-show').hide();
-        jQuery('.tim-comment-add-window').show(300);
-    }
+/* function to show place for adding comment */
+function showAddComment() {
+    jQuery('#tim-comment-add-show').hide();
+    jQuery('.tim-comment-add-window').show(300);
+}
 
-    function checkIfUserIsLoggedIn() {
-        /* function open modal when submit button is pressed without validation yet */
-        jQuery('.tim-userlogin-popup').show(300);
+function checkIfUserIsLoggedIn() {
+    /* function open modal when submit button is pressed without validation yet */
+    jQuery('.tim-userlogin-popup').show(300);
 
-        /* function to close modal login area */
-        jQuery('#tim-userlogin-popup-close').on('click', function () {
+    /* function to close modal login area */
+    jQuery('#tim-userlogin-popup-close').on('click', function () {
+        jQuery('.tim-userlogin-popup').hide();
+    });
+    /* function to close modal login area by escape button*/
+    jQuery(document).keydown(function (e) {
+        if (e.keyCode == 27) {
             jQuery('.tim-userlogin-popup').hide();
-        });
-       /* function to close modal login area by escape button*/
-        jQuery(document).keydown(function (e) {
-            if (e.keyCode == 27) {
-                jQuery('.tim-userlogin-popup').hide();
-            }
-        });        
-    }
+        }
+    });
+}
 
-    function markUserAbuse() {
-        jQuery('.tim-markabuse-popup').show(300);
+function markUserAbuse() {
+    jQuery('.tim-markabuse-popup').show(300);
 
-        jQuery('#tim-markabuse-popup-close').on('click', function () {
+    jQuery('#tim-markabuse-popup-close').on('click', function () {
+        jQuery('.tim-markabuse-popup').hide();
+        jQuery('#tim-abuse-application').show();
+        jQuery('#tim-abuse-application-sendbt').show();
+    });
+    jQuery('#tim-abuse-application-sendbt').on('click', function () {
+        jQuery('#tim-abuse-application').hide();
+        jQuery('#tim-abuse-application-sendbt').hide();
+        jQuery('.tim-markabuse-popup-container p').text('Dziękujemy za wysłanie zgłoszenia, sytuacja zostanie zbadana przez naszego pracownika w najszybszym możliwym terminie.');
+    });
+
+    jQuery(document).keydown(function (e) {
+        if (e.keyCode == 27) {
             jQuery('.tim-markabuse-popup').hide();
-			jQuery('#tim-abuse-application').show();			
-			jQuery('#tim-abuse-application-sendbt').show();				
-        });
-        jQuery('#tim-abuse-application-sendbt').on('click', function () {
-			jQuery('#tim-abuse-application').hide();			
-			jQuery('#tim-abuse-application-sendbt').hide();	
-            jQuery('.tim-markabuse-popup-container p').text('Dziękujemy za wysłanie zgłoszenia, sytuacja zostanie zbadana przez naszego pracownika w najszybszym możliwym terminie.');				
-        });
-		
-        jQuery(document).keydown(function (e) {
-            if (e.keyCode == 27) {
-                jQuery('.tim-markabuse-popup').hide();
-            }
-        });        
-    }    
+        }
+    });
+}
