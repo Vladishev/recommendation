3dbedaf63ab06e6f5298d2ec4d0578567975a607
Vlado <vladmsu@ukr.net>
2015-11-22 17:19:41 +0200

    3908 - 90%

--- app/code/local/Tim/Recommendation/Block/Adminhtml/MalpracticeReport/Grid.php
+++ app/code/local/Tim/Recommendation/Block/Adminhtml/MalpracticeReport/Grid.php
@@ -41,121 +41,63 @@ class Tim_Recommendation_Block_Adminhtml_MalpracticeReport_Grid extends Mage_Adm
             'filter' => false,
             'sortable' => false
         ));
-        /*$this->addColumn('date_add', array(
+        $this->addColumn('user_id', array(
+            'header' => Mage::helper('tim_recommendation')->__('user_id'),
+            'width' => '30',
+            'index' => 'user_id',
+            'renderer' => 'Tim_Recommendation_Block_Adminhtml_Render_CustomerName',
+            'filter' => false,
+            'sortable' => false
+        ));
+        $this->addColumn('date_add', array(
             'header' => Mage::helper('tim_recommendation')->__('date_add'),
             'width' => '10',
             'type' => 'datetime',
             'index' => 'date_add',
             'filter_index' => 'date_add',
-            'sortable' => false
-        ));
-        $this->addColumn('sku', array(
-            'header' => Mage::helper('tim_recommendation')->__('sku'),
-            'width' => '10',
-            'index' => 'sku',
-            'filter_index' => 'sku',
-            'sortable' => false
-        ));
-        $this->addColumn('value', array(
-            'header' => Mage::helper('tim_recommendation')->__('value'),
-            'width' => '30',
-            'index' => 'value',
-            'filter_index' => 'value',
-            'sortable' => false
-        ));
-        $this->addColumn('images', array(
-            'header' => Mage::helper('tim_recommendation')->__('images'),
-            'width' => '5',
-            'index' => 'recom_id',
-            'renderer' => 'Tim_Recommendation_Block_Adminhtml_Render_ProductData',
-            'filter' => false,
-            'sortable' => false
-        ));
-        $this->addColumn('user_id', array(
-            'header' => Mage::helper('tim_recommendation')->__('user_id'),
-            'width' => '10',
-            'index' => 'user_id',
-            'filter_index' => 'user_id',
-            'sortable' => false
-        ));
-        $this->addColumn('name', array(
-            'header' => Mage::helper('tim_recommendation')->__('name'),
-            'width' => '30',
-            'index' => 'name',
-            'filter_index' => 'name',
-            'sortable' => false
-        ));
-        $this->addColumn('engage', array(
-            'header' => Mage::helper('tim_recommendation')->__('engage'),
-            'width' => '10',
-            'index' => 'engage',
-            'filter_index' => 'engage',
-            'sortable' => false
-        ));
-        $this->addColumn('ocena_produktu', array(
-            'header' => Mage::helper('tim_recommendation')->__('Ocena produktu'),
-            'width' => '5',
-            'index' => 'recom_id',
-            'renderer' => 'Tim_Recommendation_Block_Adminhtml_Render_Average',
-            'filter' => false,
-            'sortable' => false
-        ));
-        $this->addColumn('rating_price', array(
-            'header' => Mage::helper('tim_recommendation')->__('rating_price'),
-            'width' => '5',
-            'index' => 'rating_price',
-            'filter_index' => 'rating_price',
-            'sortable' => false
-        ));
-        $this->addColumn('rating_durability', array(
-            'header' => Mage::helper('tim_recommendation')->__('rating_durability'),
-            'width' => '5',
-            'index' => 'rating_durability',
-            'filter_index' => 'rating_durability',
-            'sortable' => false
-        ));
-        $this->addColumn('rating_failure', array(
-            'header' => Mage::helper('tim_recommendation')->__('rating_failure'),
-            'width' => '5',
-            'index' => 'rating_failure',
-            'filter_index' => 'rating_failure',
-            'sortable' => false
-        ));
-        $this->addColumn('rating_service', array(
-            'header' => Mage::helper('tim_recommendation')->__('rating_service'),
-            'width' => '5',
-            'index' => 'rating_service',
-            'filter_index' => 'rating_service',
-            'sortable' => false
-        ));
-        $this->addColumn('advantages', array(
-            'header' => Mage::helper('tim_recommendation')->__('advantages'),
-            'width' => '100',
-            'index' => 'advantages',
-            'filter_index' => 'advantages',
-            'sortable' => false
-        ));
-        $this->addColumn('defects', array(
-            'header' => Mage::helper('tim_recommendation')->__('defects'),
-            'width' => '100',
-            'index' => 'defects',
-            'filter_index' => 'defects',
-            'sortable' => false
-        ));
-        $this->addColumn('conclusion', array(
-            'header' => Mage::helper('tim_recommendation')->__('conclusion'),
-            'width' => '100',
-            'index' => 'conclusion',
-            'filter_index' => 'conclusion',
-            'sortable' => false
+            'sortable' => true
         ));
         $this->addColumn('comment', array(
             'header' => Mage::helper('tim_recommendation')->__('comment'),
             'width' => '100',
             'index' => 'comment',
             'filter_index' => 'comment',
-            'sortable' => false
-        ));*/
+            'sortable' => true
+        ));
+        $this->addColumn('tim_ip', array(
+            'header' => Mage::helper('tim_recommendation')->__('IP'),
+            'width' => '10',
+            'index' => 'tim_ip',
+            'filter_index' => 'tim_ip',
+            'sortable' => true
+        ));
+        $this->addColumn('tim_host', array(
+            'header' => Mage::helper('tim_recommendation')->__('host'),
+            'width' => '15',
+            'index' => 'tim_host',
+            'filter_index' => 'tim_host',
+            'sortable' => true
+        ));
+        $this->addColumn('detail',
+            array(
+                'header' => Mage::helper('tim_recommendation')->__('Detail'),
+                'width' => '70',
+                'type' => 'action',
+                'getter' => 'getId',
+                'actions' => array(
+                    array(
+                        'caption' => Mage::helper('tim_recommendation')->__('Detail'),
+                        'url' => array('base' => '*/opinionReport/opinionInfo'),
+                        'target' => '_blank',
+                        'field' => 'id'
+                    )
+                ),
+                'filter' => false,
+                'sortable' => false,
+                'index' => 'stores',
+                'is_system' => true,
+            ));
+
 
         $this->addExportType('*/*/exportCsv', Mage::helper('sales')->__('CSV'));
         $this->addExportType('*/*/exportExcel', Mage::helper('sales')->__('Excel XML'));
--- app/code/local/Tim/Recommendation/Block/Adminhtml/Render/RenderCommentOpinion.php
+++ app/code/local/Tim/Recommendation/Block/Adminhtml/Render/RenderCommentOpinion.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Tim
  *
@@ -9,9 +10,22 @@
  */
 class Tim_Recommendation_Block_Adminhtml_Render_RenderCommentOpinion extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
 {
+    /**
+     * Accept recom_id and check what is it: comment or opinion.
+     * If it's opinion - returns data from 'advantages' field.
+     * If it's comment - returns data from 'comment' field.
+     * @param Varien_Object $row
+     * @return mixed
+     */
     public function render(Varien_Object $row)
     {
         $recomId = $row->getRecomId();
-        return $recomId;
+        $recomRow = Mage::getModel('tim_recommendation/recommendation')->load($recomId, 'recom_id');
+        if ($advantages = $recomRow->getAdvantages()) {
+            return $advantages;
+        } else {
+            $comment = $recomRow->getComment();
+            return $comment;
+        }
     }
 }
