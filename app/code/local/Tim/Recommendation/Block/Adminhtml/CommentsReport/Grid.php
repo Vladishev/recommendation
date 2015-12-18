4b44b82c9f19e818d37d34de7c87015e56ef1226
bdn <bakalov.bogdan@gmail.com>
2015-12-18 12:50:06 +0200

    implemented 4092 : without pop-up with recommendation info

--- app/code/local/Tim/Recommendation/Block/Adminhtml/CommentsReport/Grid.php
+++ app/code/local/Tim/Recommendation/Block/Adminhtml/CommentsReport/Grid.php
@@ -24,12 +24,13 @@ class Tim_Recommendation_Block_Adminhtml_CommentsReport_Grid extends Mage_Adminh
     protected function _prepareCollection()
     {
         $collection = Mage::getModel('tim_recommendation/recommendation')->getCollection();
-        $collection->getSelect()->join(array('val' => 'customer_entity_varchar'),
-            "val.entity_id = main_table.user_id AND val.attribute_id = (SELECT ea.attribute_id FROM eav_attribute ea JOIN eav_entity_type eat ON eat.entity_type_id = ea.entity_type_id WHERE ea.attribute_code IN ('firstname') AND eat.entity_type_code= 'customer')",
-            array("Akceptacja" => "IF (main_table.acceptance =1,'TAK','NIE')"));
-        $collection->getSelect()->join(array('val1' => 'customer_entity_varchar'),
-            "val1.entity_id = main_table.user_id AND val1.attribute_id = (SELECT ea.attribute_id FROM eav_attribute ea JOIN eav_entity_type eat ON eat.entity_type_id = ea.entity_type_id WHERE ea.attribute_code IN ('lastname') AND eat.entity_type_code= 'customer')",
-            array("name" => "CONCAT( val.value,' ', val1.value)"));
+        $collection->getSelect()->joinLeft(array('cev' => 'customer_entity_varchar'),
+            "cev.entity_id = main_table.user_id AND cev.attribute_id = 5", array('customer_firstname' => 'value'));
+        $collection->getSelect()->joinLeft(array('cev1' => 'customer_entity_varchar'),
+            "cev1.entity_id = main_table.user_id AND cev1.attribute_id = 7", array('customer_lastname' => 'value'));
+        $collection->getSelect()->joinLeft(array('tru' => 'tim_recom_user'),
+            'main_table.user_id = tru.customer_id',
+            array('nick'));
         $collection->getSelect()->where('main_table.parent IS NOT NULL');
         $this->setCollection($collection);
 
@@ -39,16 +40,22 @@ class Tim_Recommendation_Block_Adminhtml_CommentsReport_Grid extends Mage_Adminh
     protected function _prepareColumns()
     {
         $this->addColumn('recom_id', array(
-            'header' => Mage::helper('tim_recommendation')->__('ID'),
+            'header' => Mage::helper('tim_recommendation')->__('Recom ID'),
+            'width' => '10',
+            'index' => 'parent',
+            'renderer' => 'Tim_Recommendation_Block_Adminhtml_Render_RecommendDesc',
+        ));
+        $this->addColumn('comment_id', array(
+            'header' => Mage::helper('tim_recommendation')->__('Comment ID'),
             'width' => '10',
             'index' => 'recom_id',
-            'filter_index' => 'recom_id'
         ));
         $this->addColumn('name', array(
             'header' => Mage::helper('tim_recommendation')->__('Customer Name'),
             'width' => '100',
-            'index' => 'name',
-            'filter' => false
+            'index' => 'user_id',
+            'renderer' => 'Tim_Recommendation_Block_Adminhtml_Render_CustomerNameNickname',
+            'filter_index' => 'CONCAT(cev.value, \' \', cev1.value, \' \', tru.nick)'
         ));
         $this->addColumn('date_add', array(
             'header' => Mage::helper('tim_recommendation')->__('Date Added'),
@@ -61,15 +68,17 @@ class Tim_Recommendation_Block_Adminhtml_CommentsReport_Grid extends Mage_Adminh
             'header' => Mage::helper('tim_recommendation')->__('Comment'),
             'width' => '200',
             'index' => 'comment',
-            'filter' => false
         ));
-        $this->addColumn('Akceptacja', array(
-            'header' => Mage::helper('tim_recommendation')->__('Akceptacja'),
+        $this->addColumn('acceptance', array(
+            'header' => Mage::helper('tim_recommendation')->__('Acceptance'),
             'width' => '20',
-            'index' => 'Akceptacja',
-            'filter_condition_callback' => array($this, '_acceptanceFilter'),
+            'index' => 'acceptance',
+            'filter_index' => 'main_table.acceptance',
+            'type' => 'options',
+            'options' => array(0 => $this->__('No'), 1 => $this->__('Yes')),
         ));
         $this->addColumn('add_note', array(
+            'header' => Mage::helper('tim_recommendation')->__('Action'),
             'width' => '50',
             'index' => 'recom_id',
             'filter' => false,
@@ -79,48 +88,16 @@ class Tim_Recommendation_Block_Adminhtml_CommentsReport_Grid extends Mage_Adminh
         ));
         $this->addColumn('display_note',
             array(
+                'header' => Mage::helper('tim_recommendation')->__('Action'),
                 'width' => '70',
-                'type' => 'action',
-                'getter' => 'getId',
-                'actions' => array(
-                    array(
-                        'caption' => Mage::helper('tim_recommendation')->__('Display note'),
-                        'url' => array('base' => '*/noteReport'),
-                        'target' => '_blank',
-                        'field' => 'id'
-                    )
-                ),
+                'index' => 'recom_id',
+                'renderer' => 'Tim_Recommendation_Block_Adminhtml_Render_DisplayNote',
                 'filter' => false,
                 'sortable' => false,
-                'index' => 'stores',
                 'is_system' => true,
             ));
 
         return parent::_prepareColumns();
-    }
-
-    /**
-     * Custom filter for Akceptacja field
-     * @param (obj)$collection
-     * @param (obj)$column
-     * @return $this
-     */
-    protected function _acceptanceFilter($collection, $column)
-    {
-        if ($value = $column->getFilter()->getValue()) {
-            if ($value == 'TAK') {
-                $this->getCollection()->getSelect()->where(
-                    "main_table.acceptance = 1");
-                return $this;
-            }
-            if ($value == 'NIE') {
-                $this->getCollection()->getSelect()->where(
-                    "main_table.acceptance = 0");
-                return $this;
-            }
-        } else {
-            return $this;
-        }
     }
 
     protected function _prepareMassaction()
@@ -129,16 +106,26 @@ class Tim_Recommendation_Block_Adminhtml_CommentsReport_Grid extends Mage_Adminh
         $this->getMassactionBlock()->setFormFieldName('acceptance');
 
         $this->getMassactionBlock()->addItem('yes', array(
-            'label' => Mage::helper('tim_recommendation')->__('Akceptacja Tak'),
+            'label' => Mage::helper('tim_recommendation')->__('Acceptance Yes'),
             'url' => $this->getUrl('*/*/massAcceptanceYes', array('' => '')),
             'confirm' => Mage::helper('tim_recommendation')->__('Are you sure?')
         ));
         $this->getMassactionBlock()->addItem('no', array(
-            'label' => Mage::helper('tim_recommendation')->__('Akceptacja Nie'),
+            'label' => Mage::helper('tim_recommendation')->__('Acceptance No'),
             'url' => $this->getUrl('*/*/massAcceptanceNo', array('' => '')),
             'confirm' => Mage::helper('tim_recommendation')->__('Are you sure?')
         ));
 
         return $this;
+    }
+
+    /**
+     * Returns a grid URL
+     *
+     * @return string
+     */
+    public function getGridUrl()
+    {
+        return $this->getUrl('*/*/grid', array('_current' => true));
     }
 }
---
+++ app/code/local/Tim/Recommendation/Block/Adminhtml/Render/RecommendDesc.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Tim
+ *
+ * @category  Tim
+ * @package   Tim_Recommendation
+ * @copyright Copyright (c) 2015 Tim (http://tim.pl)
+ * @author    Bogdan Bakalov <bakalov.bogdan@gmail.com>
+ */
+class Tim_Recommendation_Block_Adminhtml_Render_RecommendDesc extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+{
+    public function render(Varien_Object $row)
+    {
+        $recommendationId = $row->getData($this->getColumn()->getIndex());
+        $opinion = Mage::getModel('tim_recommendation/recommendation')->load($recommendationId);
+
+        $description = $recommendationId;
+        $advantages = $opinion->getAdvantages();
+        if(!empty($advantages)){
+            if (strlen($advantages) > 100) {
+                $description .= ': ' . substr($advantages, 0, 99) . '...';
+            } else {
+                $description .= ': ' . $advantages;
+            }
+        }
+        return $description;
+    }
+}
