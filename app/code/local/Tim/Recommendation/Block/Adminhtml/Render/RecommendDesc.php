936dcda6f2e442d123f7d70deb1dd8c13370a71d
bdn <bakalov.bogdan@gmail.com>
2015-12-22 01:54:14 +0200

    changed 4092 : popup was added

--- app/code/local/Tim/Recommendation/Block/Adminhtml/Render/RecommendDesc.php
+++ app/code/local/Tim/Recommendation/Block/Adminhtml/Render/RecommendDesc.php
@@ -17,13 +17,16 @@ class Tim_Recommendation_Block_Adminhtml_Render_RecommendDesc extends Mage_Admin
 
         $description = $recommendationId;
         $advantages = $opinion->getAdvantages();
-        if(!empty($advantages)){
+        $popupUrl = '</br><a id="recomId_' . $recommendationId . '" href="javascript:displayRecommendationPopup(' . $recommendationId . ');">' . Mage::helper('tim_recommendation')->__('View') . '</a>';
+        if (!empty($advantages)) {
             if (strlen($advantages) > 100) {
                 $description .= ': ' . substr($advantages, 0, 99) . '...';
             } else {
                 $description .= ': ' . $advantages;
             }
         }
+        $description .= $popupUrl;
+
         return $description;
     }
 }
---
+++ app/code/local/Tim/Recommendation/controllers/Adminhtml/OpinionInfoController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Tim
+ *
+ * @category   Tim
+ * @package    Tim_Recommendation
+ * @copyright  Copyright (c) 2015 Tim (http://tim.pl)
+ * @author     Bogdan Bakalov <bakalov.bogdan@gmail.com>
+ */
+class Tim_Recommendation_Adminhtml_OpinionInfoController extends Mage_Adminhtml_Controller_Action
+{
+    /**
+     * Set popup content
+     */
+    public function infoPopupAction()
+    {
+        $this->loadLayout();
+        $body = $this->getLayout()->createBlock('tim_recommendation/adminhtml_opinionInfo')
+            ->setTemplate('tim/recommendation/opinionPopup.phtml')->toHtml();
+        $this->getResponse()->setBody($body);
+    }
+}
