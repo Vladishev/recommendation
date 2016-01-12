538d0c47211015ae6dd87b438cb52971f36d45a4
bdn <bakalov.bogdan@gmail.com>
2016-01-12 17:13:46 +0200

    changed 4104 : was added checking for format and size in system configuration for uploaded avatar, translations was changed

---
+++ app/code/local/Tim/Recommendation/Model/System/Config/Backend/Avatar.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * System config avatar field backend model
+ *
+ * @category   Tim
+ * @package    Tim_Recommendation
+ * @copyright  Copyright (c) 2015 Tim (http://tim.pl)
+ * @author     Bogdan Bakalov <bakalov.bogdan@gmail.com>
+ */
+class Tim_Recommendation_Model_System_Config_Backend_Avatar extends Mage_Adminhtml_Model_System_Config_Backend_File
+{
+    /**
+     * Upload max file size in kilobytes
+     *
+     * @var int
+     */
+    protected $_maxFileSize = 400;
+
+    /**
+     * Getter for allowed extensions of uploaded files
+     *
+     * @return array
+     */
+    protected function _getAllowedExtensions()
+    {
+        return array('jpg', 'jpeg', 'png');
+    }
+}
