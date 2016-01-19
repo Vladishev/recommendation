efa830e138e5211bcf17bd69304c2cd287b6d589
bdn <bakalov.bogdan@gmail.com>
2016-01-19 16:27:08 +0200

    implemented 4155

--- skin/frontend/default/default/js/tim/recommendation/recommendation.js
+++ skin/frontend/default/default/js/tim/recommendation/recommendation.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function () {
+    addOpinionAjax();
+    addCommentAjax();
+
     /* function to put value into html content right to rating stars and switch userlogin details*/
 
     jQuery('input').on('change', function () {
@@ -70,9 +73,9 @@ function markUserAbuse(id, customerId, ip, url, hostName) {
         jQuery('#tim-abuse-application-sendbt').show();
     });
     jQuery('#tim-abuse-application-sendbt').on('click', function () {
-        jQuery('#tim-abuse-application').hide();
+        jQuery('#tim-abuse-application').hide().val('');
         jQuery('#tim-abuse-application-sendbt').hide();
-        jQuery('.tim-markabuse-popup-container p').text('Dziękujemy za wysłanie zgłoszenia, sytuacja zostanie zbadana przez naszego pracownika w najszybszym możliwym terminie.');
+        jQuery('.tim-markabuse-popup-container p').text('Dziękujemy za informację o nadużyciu. Twoje zgłoszenie zostało przesłane do weryfikacji przez administratora');
     });
     recomId = id;
     userId = customerId;
@@ -97,4 +100,76 @@ function sendParams() {
         url: siteUrl,
         data: param
     });
+}
+
+/**
+ * Save opinion by AJAX
+ */
+function addOpinionAjax(){
+    jQuery("#form-validate").ajaxForm({
+        beforeSend: function() {
+            jQuery('#add-ajax-opinion').prop('disabled', true);
+            jQuery('#loading-frontend-mask').show(300);
+        },
+        success: function(response) {
+            displayAjaxOpinionPopupResponse(response);
+            jQuery("#form-validate")[0].reset();
+        },
+        error: function(response) {
+            displayAjaxOpinionPopupResponse(response);
+        }
+    });
+
+    jQuery('#tim-add-opinion-popup-close').on('click', function () {
+        jQuery('.tim-add-opinion-popup').hide();
+    });
+    jQuery(document).keydown(function (e) {
+        if (e.keyCode == 27) {
+            jQuery('.tim-add-opinion-popup').hide();
+        }
+    });
+}
+
+/**
+ * Save comment by AJAX
+ */
+function addCommentAjax(){
+    jQuery("#form-validate-comment").ajaxForm({
+        beforeSend: function() {
+            jQuery('#add-ajax-comment').prop('disabled', true);
+            jQuery('#loading-frontend-mask').show(300);
+        },
+        success: function(response) {
+            displayAjaxCommentPopupResponse(response);
+            jQuery("#form-validate-comment")[0].reset();
+        },
+        error: function(response) {
+            displayAjaxCommentPopupResponse(response);
+        }
+    });
+
+    jQuery('#tim-add-comment-popup-close').on('click', function () {
+        jQuery('.tim-add-comment-popup').hide();
+    });
+    jQuery(document).keydown(function (e) {
+        if (e.keyCode == 27) {
+            jQuery('.tim-add-comment-popup').hide();
+        }
+    });
+}
+
+function displayAjaxOpinionPopupResponse(response){
+    jQuery('#loading-frontend-mask').hide();
+    jQuery('.tim-add-opinion-popup').show(300);
+    var response = JSON.parse(response);
+    jQuery('.tim-add-opinion-popup-container p').text(response['message']);
+    jQuery('#add-ajax-opinion').prop('disabled', false);
+}
+
+function displayAjaxCommentPopupResponse(response){
+    jQuery('#loading-frontend-mask').hide();
+    jQuery('.tim-add-comment-popup').show(300);
+    var response = JSON.parse(response);
+    jQuery('.tim-add-comment-popup-container p').text(response['message']);
+    jQuery('#add-ajax-comment').prop('disabled', false);
 }
